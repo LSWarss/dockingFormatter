528d8637b7e60fe0d7d048b97e41a1216354fc2d
LSWarss <ls.warss98@gmail.com>
2020-11-15 09:53:55 +0100

    Cleaned up main.py

--- main.py
+++ main.py
@@ -10,13 +10,5 @@ args = parse.parse_args()
 
 df = DockingFormatter()
 df.findAffinityForCompound(args.s)
-# df.findAffinityForCompound("./logs/docking.log")
-# df.findAffinityForCompound("./logs/docking_1.log")
-# df.findAffinityForCompound("./logs/docking_1_1.log")
-# df.findAffinityForCompound("./logs/docking_1_2.log")
-# df.findAffinityForCompound("./logs/docking_2_1.log")
-# df.findAffinityForCompound("./logs/docking_2.log")
-# df.findAffinityForCompound("./logs/docking_APO_10_wat.log")
-# df.findAffinityForCompound("./logs/docking_APO_10.log")
-# df.findAffinityForCompound("./logs/docking_APO_wat.log")
+
 
